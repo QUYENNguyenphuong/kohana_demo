0dec34906f4e0a7f25ed998d2853cf5e30ef8b1c
QUYENNguyenphuong <quyen.nguyen@ceresolutions.com>
2018-11-12 09:08:05 +0700

    update routing

--- kohana-v3.3.6/application/views/User/Create.php
+++ kohana-v3.3.6/application/views/User/Create.php
@@ -3,9 +3,11 @@
 <form method="post" action="">
 <lable>User name: </lable>
 <input type="text" name="username" >
+<span class="text-danger"> <?= isset($name) ? $name : '' ?> </span>
 <br>
 <label>Email </label>
 <input type="text" name="email" >
+<span class="text-danger"> <?= isset($email) ? $email : '' ?> </span>
 <br>
 <input type="submit" name="btn_create" value="Create">
 </form>
